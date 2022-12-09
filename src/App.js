a7fdcdbd88ee06bd0bcd5d7a86880a000bba7643
Zachary Soliman <zachsoliman1@gmail.com>
2022-12-08 20:02:07 -0500

    ternary is correct

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
 
   //   console.log(hidebutton(true))
 
-  const showProjectDescription = useState(true)
+  const [projectDescription, setProjectDescription] = useState(false)
 
   return (
 
@@ -112,7 +112,7 @@ const App = () => {
             <img className='projectimg' src={drinkupnav} />
           </a>
 
-          <div className={`projectCard${showProjectDescription ? ' hidden' : ''}`}>
+          <div className={`projectCard${projectDescription ? ', hidden' : ''}`}>
             <a>Created as a final project <br />at the Flatiron School.</a> <br />
             <a>Cocktail API-based user <br /> interface to search drink <br /> recipies and save liked <br /> selections.</a> <br />
             <a>React.js front end, utilizing <br /> Redux and an outside API.</a><br />
